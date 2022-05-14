a098ab379e2d4e82e3e26dedd887748483d63ada
Jin0811 <15797954826@163.com>
2022-05-14 17:27:38 +0800

    添加标签模板的注释

--- reactDemo/src/06_styled-components/01_base.jsx
+++ reactDemo/src/06_styled-components/01_base.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import styled from "styled-components";
 
+// const StyledFooter = styled.footer``; 这种函数后面跟模板字符串的语法，称为标签模板
+// 模板字符串可以紧跟在一个函数名后面，该函数将被调用来处理这个模板字符串，这被称为“标签模板”功能（tagged template）
+// 参考：https://es6.ruanyifeng.com/#docs/string
 const StyledFooter = styled.footer`
   background: #f1f1f1;
   width: 50%;
